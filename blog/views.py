c5aa2a6da8106b76c080c0d1a8524ff89792cacc
0311snitch <0311snitch@gmail.com>
2018-04-06 00:08:41 +0300

    added login module

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,17 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    users = User.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts, 'users': users})
+
+def a_post_list(request, pk):
+	posts = Post.objects.filter(author=pk)
+	return render(request, 'blog/a_post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -39,3 +46,29 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	Post.objects.filter(title=post.title).delete()
+	return redirect('post_list')
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Правильный пароль и пользователь "активен"
+        auth.login(request, user)
+        # Перенаправление на "правильную" страницу
+        return redirect('post_list')
+    else:
+        # Отображение страницы с ошибкой
+        return HttpResponseRedirect("/account/invalid/")
+
+
+def logout(request):
+    auth.logout(request)
+    # Перенаправление на страницу.
+    return redirect('post_list')
